5ffe80f3fc8ae49c8ce5f7f76ba53abc123d5a5c
khalimzoda <khalimzodakomil@gmail.com>
2025-09-14 18:43:51 +0200

    Add files via upload

---
+++ Password_generator/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(208, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 47);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(548, 117);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(177, 224);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(544, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(558, 356);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 22);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(26, 68);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(26, 201);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(126, 24);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(26, 260);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 24);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(26, 317);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(126, 24);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 816);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Password_generator/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1: Form
+    {
+        private Random random = new Random();
+    
+        private const string UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string LowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        private const string Numbers = "0123456789";
+        private const string SpecialChars = "#&@?%!'_+/";
+
+        // Declare checkboxes as class-level fields
+        private CheckBox checkBoxRequireUppercase;
+        private CheckBox checkBoxRequireNumber;
+        private CheckBox checkBoxRequireSpecialChar;
+        private Label labelMeetsLength;
+        private Label labelHasUppercase;
+        private Label labelHasNumber;
+        private Label labelHasSpecial;
+        private NumericUpDown numericUpDownMinLength;
+        private Label labelMinLength;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCheckBoxes();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Length";
+            label2.Text = "Your new password:";
+            label3.Text = "Password generator";
+            label4.Text = "Password suggestion history";
+            button1.Text = "Generate";
+            radioButton1.Text = "Upper+lowercase";
+            radioButton2.Text = "Contains numbers";
+            radioButton3.Text = "Contains special characters";
+            radioButton1.Checked = true;
+            label5.AutoSize = false;
+            label5.TextAlign = ContentAlignment.MiddleLeft;
+            label5.Text = "The selected line will be automatically copied into the main Windows clipboard";
+            label5.Width = 90;
+            label5.Height = 90;
+
+        }
+
+        private void InitializeCheckBoxes()
+        {
+            checkBoxRequireUppercase = new CheckBox { Text = "Include Uppercase Letters", Left = 180, Top = 128, Width = 200 };
+            checkBoxRequireNumber = new CheckBox { Text = "Include Numbers", Left = 180, Top = 165, Width = 200 };
+            checkBoxRequireSpecialChar = new CheckBox { Text = "Include Special Characters", Left = 180, Top = 202, Width = 250 };
+           labelMeetsLength = new  Label { Text = "Meets Length Requirement ❌", Left = 180, Top = 249, Width = 250, ForeColor = System.Drawing.Color.Gray };
+            labelHasUppercase = new Label { Text = "Contains Uppercase Letter ❌", Left = 180, Top = 286, Width = 250, ForeColor = System.Drawing.Color.Gray };
+            labelHasNumber = new Label { Text = "Contains Number ❌", Left = 180, Top = 323, Width = 250, ForeColor = System.Drawing.Color.Gray };
+            labelHasSpecial = new Label { Text = "Contains Special Character ❌", Left = 180, Top = 360, Width = 250, ForeColor = System.Drawing.Color.Gray };
+
+            labelMinLength = new Label { Text = "Minimal Length", Left = 15, Top = 77 };
+            numericUpDownMinLength = new NumericUpDown { Left = 15, Top = 97, Minimum = 4, Maximum = 20, Value = 8 };
+      
+
+            Controls.Add(checkBoxRequireUppercase);
+            Controls.Add(checkBoxRequireNumber);
+            Controls.Add(checkBoxRequireSpecialChar);
+            Controls.Add(labelMeetsLength);
+            Controls.Add(labelHasUppercase);
+            Controls.Add(labelHasNumber);
+            Controls.Add(labelHasSpecial);
+            Controls.Add(labelMinLength);
+            Controls.Add(labelMinLength);
+            Controls.Add(numericUpDownMinLength);
+
+
+        }
+
+
+     
+        private void createRandomPassword()
+        {
+            int length = (int)numericUpDown1.Value;
+            int minLength = (int)numericUpDownMinLength.Value;
+
+            if (length < minLength)
+            {
+                MessageBox.Show($"Password length must be at least {minLength}", "Invalid Length", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            List<char> passwordChars = new List<char>();
+            string charPool = "";
+
+            if (radioButton1.Checked)
+            {
+                charPool = UppercaseLetters + LowercaseLetters;
+            }
+            else if (radioButton2.Checked)
+            {
+                charPool = Numbers;
+            }
+            else if (radioButton3.Checked)
+            {
+                charPool = SpecialChars;
+            }
+
+            if (checkBoxRequireUppercase.Checked)
+            {
+                charPool += UppercaseLetters;
+                passwordChars.Add(UppercaseLetters[random.Next(UppercaseLetters.Length)]);
+            }
+            if (checkBoxRequireNumber.Checked)
+            {
+                charPool += Numbers;
+                passwordChars.Add(Numbers[random.Next(Numbers.Length)]);
+            }
+            if (checkBoxRequireSpecialChar.Checked)
+            {
+                charPool += SpecialChars;
+                passwordChars.Add(SpecialChars[random.Next(SpecialChars.Length)]);
+            }
+            if (!radioButton1.Checked && !radioButton2.Checked && !radioButton3.Checked)
+            {
+                charPool += LowercaseLetters;
+            }
+
+            while (passwordChars.Count < length)
+            {
+                passwordChars.Add(charPool[random.Next(charPool.Length)]);
+            }
+
+            passwordChars = passwordChars.OrderBy(_ => random.Next()).ToList();
+            string generatedPassword = new string(passwordChars.ToArray());
+
+            textBox1.Text = generatedPassword;
+            listBox1.Items.Add(generatedPassword);
+
+            validatePassword(generatedPassword);
+        }
+
+        private void validatePassword(string password)
+        {
+            bool meetsLength = password.Length >= numericUpDownMinLength.Value;
+            bool hasUppercase = password.Any(char.IsUpper);
+            bool hasNumber = password.Any(char.IsDigit);
+            bool hasSpecial = password.Any(ch => SpecialChars.Contains(ch));
+
+            labelMeetsLength.Text = meetsLength ? "Meets Length Requirement ✅" : "Meets Length Requirement ❌";
+            labelMeetsLength.ForeColor = meetsLength ? System.Drawing.Color.Green : System.Drawing.Color.Red;
+
+            labelHasUppercase.Text = hasUppercase ? "Contains Uppercase Letter ✅" : "Contains Uppercase Letter ❌";
+            labelHasUppercase.ForeColor = hasUppercase ? System.Drawing.Color.Green : System.Drawing.Color.Red;
+
+            labelHasNumber.Text = hasNumber ? "Contains Number ✅" : "Contains Number ❌";
+            labelHasNumber.ForeColor = hasNumber ? System.Drawing.Color.Green : System.Drawing.Color.Red;
+
+            labelHasSpecial.Text = hasSpecial ? "Contains Special Character ✅" : "Contains Special Character ❌";
+            labelHasSpecial.ForeColor = hasSpecial ? System.Drawing.Color.Green : System.Drawing.Color.Red;
+
+        }
+
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedPassword = listBox1.SelectedItem.ToString();
+                Clipboard.SetText(selectedPassword);
+                validatePassword(selectedPassword);
+            }
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            createRandomPassword();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+       
+       
+        }
+    
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+       
+        
+         
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+           
+            
+        }
+    }
+}
+
